6e913be7001df4d541ce909c07023c665e16437d
Josh Ortiz <37316797+JoshChubi@users.noreply.github.com>
2019-04-15 22:58:14 -0700

    Create poundToKg.cpp

---
+++ Fall2014/Intro to C++/Pounds to kg/poundToKg.cpp
@@ -0,0 +1,11 @@
+// CS 575, HW#1A, Josh Ortiz
+#include <iostream> 
+using namespace std;
+
+int main(){
+	double pounds, kg;
+	cout <<"How many pounds do you weigh? ";
+	cin >>pounds;
+	kg = pounds / 2.205;
+	cout <<"That's " <<kg <<" kilograms" <<endl;
+} 
