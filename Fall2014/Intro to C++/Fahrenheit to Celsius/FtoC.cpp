c1d1050f47131faa93ce95938d8b384cdaabc4e0
Josh Ortiz <37316797+JoshChubi@users.noreply.github.com>
2019-04-15 23:00:44 -0700

    Create FtoC.cpp

---
+++ Fall2014/Intro to C++/Fahrenheit to Celsius/FtoC.cpp
@@ -0,0 +1,11 @@
+// CS 575 Josh Ortiz
+#include <iostream>
+using namespace std;
+
+int main(){
+    double cel, fah;
+    cout <<"Fahrenheit temperature: ";
+    cin >>fah;
+   c = (f - 32)* 5/9;
+    cout <<f <<" Deg F = " <<c <<" Celsius" <<endl;
+} 
